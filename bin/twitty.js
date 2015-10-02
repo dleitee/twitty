ceae07aeaef02bda61c877309ddb5f18859f0043
Daniel Leite de Oliveira <dleitee@gmail.com>
2015-10-02 19:47:10 -0300

    Readline-sync

--- bin/twitty.js
+++ bin/twitty.js
@@ -1,3 +1,14 @@
 #! /usr/bin/env node
 
 console.log(process.argv);
+
+var readlineSync = require('readline-sync');
+ 
+// Wait for user's response. 
+var userName = readlineSync.question('Username: ');
+ 
+// Handle the secret text (e.g. password). 
+var password = readlineSync.question('Password: ', {
+  hideEchoBack: true
+});
+console.log('User:' + userName + ' Password: ' + password + '!');
